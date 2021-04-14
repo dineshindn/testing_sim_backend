39b7085bfa1125019af0c9c57a2b9480b322ef65
nelsonraj A <nelsonarokiaraj@nyl.technology>
2021-04-14 11:23:51 +0530

    updated the sim Swap api

--- modules/simSwap/index.js
+++ modules/simSwap/index.js
@@ -15,6 +15,8 @@ module.exports = {
           new Date()
         ]
       );
+      await executeQuery(`UPDATE simDetails SET deviceId=? WHERE id=?`,[req.body.deviceId, req.body.fk_newSim]);
+      await executeQuery(`UPDATE simDetails SET deviceId=? WHERE id=?`,['', req.body.fk_oldSim]);
       return res.send({ status: 200, message: 'success', reason: 'Sim Swapped Successfully', result: { id: result.insertId} });
     } catch (err) {
       return res.send({ status: 400, message: 'failure', result: { error: err.message } });
@@ -23,7 +25,8 @@ module.exports = {
 
   async list(req, res) {
     try {
-      const result = await executeQuery("SELECT * FROM `simSwap`");
+      const result = await executeQuery("SELECT simSwap.*, users.firstName, simDetails.deviceId as oldSimDeviceId FROM `simSwap` LEFT JOIN `users` ON simSwap.fk_createdBy = users.id LEFT JOIN `simDetails` ON fk_oldSim = simDetails.id" );
+      //const result = await executeQuery("SELECT * FROM `simSwap`");
       return res.status(200).send({ data: result });
     } catch (err) {
       return res.status(500).send({ error: err });
