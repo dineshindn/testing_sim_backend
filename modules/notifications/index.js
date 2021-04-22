3e74898fef02a5a6cabb68e7c6f3aa31a9d0c992
nelsonraj A <nelsonarokiaraj@nyl.technology>
2021-04-22 10:15:07 +0530

    error response added

--- modules/notifications/index.js
+++ modules/notifications/index.js
@@ -4,6 +4,8 @@ module.exports = {
   async list(req, res) {
     try {
       const result = await executeQuery("SELECT notifications.*, userRequests.requestNumber FROM `notifications` LEFT JOIN `userRequests` ON notifications.fk_userRequestsId = userRequests.id " );
+
+      // const result = await executeQuery("SELECT notifications.*, userRequests.requestNumber, users.firstName AS createdByName FROM `notifications` LEFT JOIN `userRequests` ON notifications.fk_userRequestsId = userRequests.id LEFT JOIN `users` ON notifications.fk_createdBy = users.id AND fk_resolvedBy" );
       return res.status(200).send({ status: 200, message: 'Success', data: result });
     } catch (err) {
       return res.status(400).send({ status: 400, message: 'failure', reason: 'something went wrong', error: err.message });
@@ -13,7 +15,7 @@ module.exports = {
   async markAsRead(req, res) {
     try {
       const notifiyId = req.body && req.body.notificationId ? req.body.notificationId : '';
-      if (!notifiyId) return res.status(400).send({ status: 400, message: 'failure', reason: 'Invalid notificationId', error: err.message });
+      if (!notifiyId) return res.status(400).send({ status: 400, message: 'failure', reason: 'Invalid notificationId' });
       await executeQuery(`UPDATE notifications SET isRead=?, updateUTC=? WHERE id=?`, [1, new Date(), notifiyId]);
       return res.status(200).send({ status: 200, message: 'Success' });
     } catch (err) {
