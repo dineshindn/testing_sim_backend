ed4ab4e2bd143904810509c8d41598486b92aaab
nelsonraj A <nelsonarokiaraj@nyl.technology>
2021-04-22 08:50:33 +0530

    request number added in the notifications response

--- modules/notifications/index.js
+++ modules/notifications/index.js
@@ -3,9 +3,7 @@ const executeQuery = require('../../database');
 module.exports = {
   async list(req, res) {
     try {
-        const result = await executeQuery("SELECT * FROM `notifications`");
-      //   const result = await executeQuery("SELECT notifications.*, users.firstName AS createdByName FROM `notifications` LEFT JOIN `users` ON notifications.fk_createdBy = users.id " );
-
+      const result = await executeQuery("SELECT notifications.*, userRequests.requestNumber FROM `notifications` LEFT JOIN `userRequests` ON notifications.fk_userRequestsId = userRequests.id " );
       return res.status(200).send({ status: 200, message: 'Success', data: result });
     } catch (err) {
       return res.status(400).send({ status: 400, message: 'failure', reason: 'something went wrong', error: err.message });
