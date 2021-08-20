1a59ea65208a2f6768f7f1d0b44af76b8d5e6f73
Dinesh <51910892+dineshindn@users.noreply.github.com>
2021-08-20 13:39:19 +0530

    Update app.js

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.use(json2xls.middleware);
 
 // simple route
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to backends" });
+  res.json({ message: "Welcome to backends1" });
 });
 
 // Require our routes into the application.
