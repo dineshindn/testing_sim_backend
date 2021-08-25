9c942ee15d49c10b35164b1e7c05f084ccff2ba5
Dinesh <51910892+dineshindn@users.noreply.github.com>
2021-08-25 10:55:38 +0530

    Update migrations.js

--- migrations.js
+++ migrations.js
@@ -3,8 +3,7 @@ const executeQuery = require("./database");
 module.exports = (app) => {
 
     try{
-    
-        const result = executeQuery(`ALTER TABLE oem ADD test VARCHAR(10) NOT NULL AFTER updateUTC;`);
+        const result = executeQuery("ALTER TABLE oem ADD test VARCHAR(10) NOT NULL AFTER updateUTC;");
         const result = executeQuery("ALTER TABLE `oem` CHANGE `description` `description` VARCHAR(200)");
           console.log("table created ...!! "+ result );
     }catch{
