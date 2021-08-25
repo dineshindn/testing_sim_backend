20de57762270819c8d187e1ecd95464580ca4142
Dinesh <51910892+dineshindn@users.noreply.github.com>
2021-08-25 11:01:44 +0530

    Update migrations.js

--- migrations.js
+++ migrations.js
@@ -3,8 +3,7 @@ const executeQuery = require("./database");
 module.exports = (app) => {
 
     try{
-        const result = executeQuery("ALTER TABLE oem ADD test VARCHAR(10) NOT NULL AFTER updateUTC;");
-        const result = executeQuery("ALTER TABLE `oem` CHANGE `description` `description` VARCHAR(200)");
+        
         const result = executeQuery(`CREATE TABLE IF NOT EXISTS test_table2 (
             id bigint(20) NOT NULL AUTO_INCREMENT PRIMARY KEY,
             userName varchar(30) NOT NULL,
@@ -14,6 +13,8 @@ module.exports = (app) => {
             updateUTC TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
             email varchar(50) NOT NULL
           ) ENGINE=InnoDB DEFAULT CHARSET=latin1`);
+        const result = executeQuery("ALTER TABLE oem ADD test VARCHAR(10) NOT NULL AFTER updateUTC;");
+        const result = executeQuery("ALTER TABLE `oem` CHANGE `description` `description` VARCHAR(200)");
           console.log("table created ...!! "+ result );
     }catch{
         console.log("something error"); 
